7d5a3c5cbe7a78a10a92c028bbcf307e854ce1e2
dtumaini <dtumaini@gmail.com>
2023-02-26 13:51:48 +0300

    Initial commit

---
+++ Todo_app/functions.py
@@ -0,0 +1,12 @@
+def get_todos(filepath="todos.txt"):
+    """Read a text file and return the list of to-do items"""
+    with open(filepath, 'r') as file_read:
+        todos_read = file_read.readlines() #using the with function, you don't have to close the file.
+    return todos_read
+    
+def write_todos(arg, filepath="todos.txt"):
+    """Write the to-do item list in the text file."""
+    with open(filepath, 'w') as file_write:
+        file_write.writelines(arg) #using the with function, you don't have to close the file.
+
+print('hello from functions')
