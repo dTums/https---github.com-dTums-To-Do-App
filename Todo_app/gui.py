579a6084370dcf0b83698d25e11caf5f35bdbe15
dtumaini <dtumaini@gmail.com>
2023-03-22 11:04:21 +0300

    d

--- Todo_app/gui.py
+++ Todo_app/gui.py
@@ -9,10 +9,13 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Close")
 
 window = sg.Window ('Tums To-Do app',  
                     layout=[[label], [input_box, add_button],
-                            [list_box, edit_button]], 
+                            [list_box, edit_button, complete_button],
+                            [exit_button]], 
                     font =('Helvetika', 20))
 
 
@@ -37,6 +40,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todos_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todos_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Close":
+            break
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
