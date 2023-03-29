0a37637f5903aec81781943cab4b5451e290891b
dtumaini <dtumaini@gmail.com>
2023-03-29 15:11:04 +0300

    create a static web interface

---
+++ Todo_app/WebTodo.py
@@ -0,0 +1,13 @@
+import streamlit as st #new intuitive library for creating web apps and integrate well with graphs. also easy to deploy
+import functions
+
+todos = functions.get_todos()
+
+st.title("Tums ToDo App")
+st.subheader("This is my Todo App.")
+st.write("This app hopes to increase your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder= "Add new todo... ")
