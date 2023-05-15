4e6fd91607bb7d240c68609096ebdd2d451ed2df
dtumaini <dtumaini@gmail.com>
2023-05-15 12:11:53 +0300

    Day 2 Add app description, image and links

--- app2-portfolio/main.py
+++ app2-portfolio/main.py
@@ -25,14 +25,20 @@ Feel free to contact me for any opportunities to work together.
 
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.3, 1.5])
 
 df = pd.read_csv('data.csv', sep=";")
 
 with col3:
     for index, row  in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Link]({row['url']})")
 
 with col4:
     for index, row  in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Link]({row['url']})")
