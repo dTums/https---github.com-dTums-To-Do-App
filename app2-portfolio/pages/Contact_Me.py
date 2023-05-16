8c29fe0b4c0603625aff489eb477fadd30b01078
dtumaini <dtumaini@gmail.com>
2023-05-16 16:09:30 +0300

    Day 3 Send emails using the form

--- app2-portfolio/pages/Contact_Me.py
+++ app2-portfolio/pages/Contact_Me.py
@@ -1,11 +1,20 @@
 import streamlit as st
+from my_email import send_email
 
 st.header("Contact Me")
 
-with st.form(key='email_form'):
+with st.form(key='email_forms'):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your Message")
+    raw_message = st.text_area("Your Message")
+    message = f"""\
+Subject: New email from {user_email} 
+
+From : {user_email} 
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        print("Thank you for your message. I'll get back to you as soon as i can.")
+        send_email(message)
+        st.info("Your message has been sent. I will get back to you as soon as possible")
+
 
