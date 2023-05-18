a65868ba5dcc9101ea3a687ed2e7357058c5bdd9
dtumaini <dtumaini@gmail.com>
2023-05-18 17:01:43 +0300

    Add lines on each page

--- app4-pdf generation/main.py
+++ app4-pdf generation/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 pdf = FPDF(orientation="p", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
+
 df = pd.read_csv('topics.csv')
 
 for index, row in df.iterrows():
@@ -13,13 +14,18 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+    # add lines to master pages
+    for y in range (21, 289, 10):
+        pdf.line(10, y, 200, y)
+
 
     # set the footer for the master page
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=10)
     pdf.set_text_color(150, 150, 150)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+
 
 
     for i in range(row["Pages"]-1):
@@ -30,5 +36,10 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(150, 150, 150)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        pdf.line(10, 289, 200, 289)
+
+        # add lines to other pages
+        for y in range(21, 289, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
